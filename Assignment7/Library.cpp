65129fbed6a76897f1260d038f665d4e38af1dda
Anderson Taylor <andltayl@ut.utm.edu>
2018-11-19 16:33:33 -0600

    All functions but author_search and remove

--- Assignment7/Library.cpp
+++ Assignment7/Library.cpp
@@ -4,18 +4,54 @@
 
 Library::Library()
 {
-
+	// Not Needed.
 }
 
 
 Library::~Library()
 {
-
+	// Not Needed.
 }
 
 void Library::insert_sorted(book addBook)
 {
+	list<book>::iterator it = books.begin();
+	int currChar = 0;
+	int shortestTitle = 0;
 
+	if (books.empty()) {										// Check if this is the first book to be added to the library.
+		books.push_back(addBook);								// If so then add it and return.
+		return;
+	}
+
+	if (addBook.title.length() < it->title.length()) {			// In case of duplicate titles find the shorter one.
+		shortestTitle = addBook.title.length();
+	}
+	else if (addBook.title.length() > it->title.length()) {
+		shortestTitle = it->title.length();
+	}
+	else {														// If same length.
+		shortestTitle = addBook.title.length();
+	}
+	
+	while (currChar < shortestTitle) {							// Compare each char until one is bigger, up until the last char of the shortest title.
+		if (it == books.end()) {								// If we are at the end of the list just insert the book.
+			books.push_back(addBook);
+			return;
+		}
+		else if(addBook.title[currChar] < it->title[currChar]) {	// If the new book is less insert it where the iterator currently is.
+			books.insert(it, addBook);
+			return;
+		}
+		else if (addBook.title[currChar] > it->title[currChar]) {	// If the new book is bigger go to next book in list.
+			it++;
+		}
+		else {														// If the chars are equal then go to the next char in each title and loop again.
+			currChar++;
+		}
+	}
+	books.insert(it, addBook);		// If the titles are the same up to the last char of the shortest title insert the book before the longer one.
+	return;
 }
 
 void Library::read_from_file(string filename)
@@ -31,10 +67,10 @@ void Library::read_from_file(string filename)
 
 		getline(iFile, temp.title);
 		
-		while (loop){
+		while (loop){										// Add the multiple author names if there are more than one.
 			getline(iFile, tempName);
 			
-			if (tempName[tempName.size() - 1] == '*') {
+			if (tempName[tempName.size() - 1] == '*') {		// Remove the star from the last author name.
 				tempName.pop_back();
 				loop = false;
 			}
@@ -47,7 +83,7 @@ void Library::read_from_file(string filename)
 
 		insert_sorted(temp);
 
-		iFile.get();
+		iFile.get();		// Throw away newline character.
 	}
 
 	iFile.close();
@@ -59,6 +95,16 @@ void Library::print()
 
 	while (temp != books.end()) {
 		cout << "Title: " << temp->title << '\n';
+		cout << "Authors: " << endl;
+
+		list<string>::iterator it;
+
+		for (it = temp->aNames.begin(); it != temp->aNames.end(); it++) {		// Print all author names.
+			cout << *it << endl;
+		}
+
+		cout << "Year Published: " << temp->year << '\n';
+
 		cout << fixed << setprecision(2) << "Price: $" << temp->price << '\n' << endl;
 
 		temp++;
@@ -67,7 +113,16 @@ void Library::print()
 
 void Library::find_book(string name)
 {
+	list<book>::iterator temp = books.begin();
+	Library results;
 
+	while (temp != books.end()) {							// Search all the books in the library for the string.
+		if (temp->title.find(name) != std::string::npos) {
+			results.insert_sorted(*temp);
+		}
+		temp++;
+	}
+	results.print();
 }
 
 void Library::author_search(string name)
@@ -82,18 +137,31 @@ void Library::remove(string name)
 
 void Library::write_to_file(string filename)
 {
-	/*
 	ofstream oFile;
-	oFile.open(file.c_str());
-	node * temp = head;
+	oFile.open(filename.c_str());
 
-	while (temp != NULL) {
-		oFile << '\n' << temp->first_name << " " << temp->last_name << '\n';
-		oFile << fixed << setprecision(2) << temp->salary;
+	list<book>::iterator temp = books.begin();
 
-		temp = temp->next;
+	while (temp != books.end()) {						// Works essentially the same as the print function.
+		if (temp != books.begin())
+		{
+			oFile << endl;
+		}
+		oFile << temp->title;
+
+		list<string>::iterator it;
+
+		for (it = temp->aNames.begin(); it != temp->aNames.end(); it++) {
+			oFile << '\n' << *it;
+		}
+		oFile << '*' << '\n';							// Replace the star to indicate the final author name in the file.
+		
+		oFile << temp->price << endl;
+		oFile << temp->year;
+
+		temp++;
 	}
 
 	oFile.close();
-	*/
+	
 }
